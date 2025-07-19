7a1bf528e27a2895d8105e522ed069271cc5cfbb
WhatDidYouExpect <89535984+WhatDidYouExpect@users.noreply.github.com>
2025-07-19 14:34:03 +0200

    openrouter! never heard of her!

--- modules/modes.py
+++ modules/modes.py
@@ -1,5 +1,4 @@
 import time
-import modules.ai as ai
 from modules.config.configuration import config
 from modules.utils import parse_have_want, print_new_post
 
@@ -18,22 +17,3 @@ def match(subreddit):
         h, w = parse_have_want(submission.title)
         if any(s in h.lower() for s in author_has_lower) and any(s in w.lower() for s in author_wants_lower):
             print_new_post(subreddit, submission.author, h, w, submission.url, submission.created_utc, submission.author_flair_text, submission.title)
-
-def match_llm(subreddit):
-    try:
-        openrouter = ai.OpenRouter()
-        
-        for submission in subreddit.stream.submissions(skip_existing = not config.retrieve_older_posts):
-            h, w = parse_have_want(submission.title)
-            title_replaced: str = str(submission.title).replace("[H]", "[Have]").replace("[W]", "[Want]").strip()
-            
-            response = openrouter.llm(author_has_llm_query=config.author_has_llm_query, author_wants_llm_query=config.author_wants_llm_query, title=title_replaced)
-
-            if openrouter.is_match(response):
-                print_new_post(subreddit, submission.author, h, w, submission.url, submission.created_utc, submission.author_flair_text, submission.title)
-            
-            # sleep for 3 seconds to avoid API rate limiting, which would crash the script
-            time.sleep(3)
-            
-    except Exception as e:
-        raise Exception(f"{e.__dict__['body']['message']}")
